cbdea6d20fb8936707e6b36f39af38176d627a63
Marius Apriutesa <mariusapriutesa@gmail.com>
2022-02-01 21:30:57 +0100

    Agregar archivos de proyecto.

---
+++ InformesEmpleado/DataSet/DataSetHandler.cs
@@ -0,0 +1,23 @@
+﻿using InformesEmpleado.DataSet._2dam03DataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformesEmpleado.DataSet
+{
+    class DataSetHandler
+    {
+        private static InformeTableAdapter adapter = new InformeTableAdapter();
+
+        public static void GetByIdDpto(int id_dpto) {
+
+            var data = adapter.GetDataByIdDpto(id_dpto);
+        
+        }
+
+
+
+            }
+}
